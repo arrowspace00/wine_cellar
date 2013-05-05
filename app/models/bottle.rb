d2194b1644e96cf02a8adf567ff1d7ff19c95151
Mark Ross <mross3@amatlrzrmross3.local>
2013-05-05 12:16:19 -0400

    SQLLite 3, plus releated gitignores, etc

--- app/models/bottle.rb
+++ app/models/bottle.rb
@@ -1,40 +1,37 @@
 class Bottle < ActiveRecord::Base
 
-# Mass Assignment 
-  attr_accessible :alcohol, :appellation, :bin, :loc, :cases, :country, :main_region, :main_varietal, :name, :photo, :price, 
+	# Mass Assignment 
+	attr_accessible :alcohol, :appellation, :bin, :loc, :cases, :country, :main_region, :main_varietal, :name, :photo, :price, 
 							:profile, :rp, :second_varietal, :style_type, :sub_region, :third_varietal, :vintage, :winery, :ws
 
 							
-# Model Strengthened by Validation
-	validates :name, :presence => true, 
-								:length => { :minimum => 1 }
+	# Model Strengthened by Validation
+	validates :name, :presence => true, :length => { :minimum => 1 }
 								
-	validates :bin, :presence => true, 
-								:length => { :minimum => 4, :maximum => 4 }
+	validates :bin, :presence => true, :length => { :minimum => 4, :maximum => 4 }
 								
-	validates_uniqueness_of :bin, 
-								:message => "- - This bin# is already in use. Please choose a bin# that is not being used."
+	validates_uniqueness_of :bin, :message => "- - This bin# is already in use. Please choose a bin# that is not being used."
 																		  
 	validates_numericality_of :bin,
-								:allow_nil => false,
-								:greater_than => 999,
-								:less_than => 10000,
-								:message => "- Please enter a four-digit value between 1000-9999"
+		:allow_nil => false,
+		:greater_than => 999,
+		:less_than => 10000,
+		:message => "- Please enter a four-digit value between 1000-9999"
 	
-def previous
-	Bottle.limit(1).order("id DESC").where("id < ?", id).first
-end
-
-def next
-	Bottle.limit(1).order("id DESC").where("id > ?", id).first
-end
-
-
-def self.import(file)
-	CSV.foreach(file.path, headers: true) do |row|
-		Bottle.create! row.to_hash
+	def previous
+		Bottle.limit(1).order("id DESC").where("id < ?", id).first
 	end
-end
+
+	def next
+		Bottle.limit(1).order("id DESC").where("id > ?", id).first
+	end
+
+
+	def self.import(file)
+		CSV.foreach(file.path, headers: true) do |row|
+			Bottle.create! row.to_hash
+		end
+	end
 
 	
 	# after the bottle has been written to the database, 
@@ -64,14 +61,16 @@ end
 	end
 
 	
-private
-	
+	private
 	def store_photo
+		logger.info "========================================== Storing Photo:"
+		logger.info @file_data
 		if @file_data
 			FileUtils.mkdir_p PHOTO_STORE
 			File.open(photo_filename, 'wb') do |f|
 				f.write(@file_data.read)
 				end
+
 			@file_data = nil
 		end
 	end
